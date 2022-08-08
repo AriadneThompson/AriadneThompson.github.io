681af8a60d246ba36e7a001fdf3df726a0327aa9
Ariadne Thompson <AriadneThompson@protonmail.com>
2022-08-08 11:20:13 +0100

    Fixed case sensitivity issue (hopefully)

--- HTMLify.py
+++ HTMLify.py
@@ -16,15 +16,15 @@ MATHJAX = 4
 
 
 pages = ({FILENAME: "index", TITLE: "Ariadne's Website", RETURNTO: None, RETURNTEXT: None, MATHJAX: False},
-         {FILENAME: "Writing", TITLE: "Ariadne's Writing", RETURNTO: "index", RETURNTEXT: "Return to Main Page", MATHJAX: False},
-         {FILENAME: "Contact_Me", TITLE: "Contact Me", RETURNTO: "index", RETURNTEXT: "Return to Main Page", MATHJAX: False},
-         {FILENAME: "A_Mountain_Inverted", TITLE: "A Mountain, Inverted", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
-         {FILENAME: "Practical_Applications_of_Fluid_Dynamics", TITLE: "Practical Applications of Fluid Dynamics", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
-         {FILENAME: "Question_One", TITLE: "Question One", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: True},
-         {FILENAME: "Question_One_Screen_Reader_Friendly", TITLE: "Question One", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
-         {FILENAME: "Wretched_Creatures", TITLE: "Wretched Creatures", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
-         {FILENAME: "The_Human_Nature", TITLE: "The Human Nature", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
-         {FILENAME: "Fifty_Princesses", TITLE: "Fifty Princesses", RETURNTO: "Writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "writing", TITLE: "Ariadne's Writing", RETURNTO: "index", RETURNTEXT: "Return to Main Page", MATHJAX: False},
+         {FILENAME: "contact_me", TITLE: "Contact Me", RETURNTO: "index", RETURNTEXT: "Return to Main Page", MATHJAX: False},
+         {FILENAME: "a_mountain_inverted", TITLE: "A Mountain, Inverted", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "practical_applications_of_fluid_dynamics", TITLE: "Practical Applications of Fluid Dynamics", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "question_one", TITLE: "Question One", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: True},
+         {FILENAME: "question_one_screen_reader_friendly", TITLE: "Question One", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "wretched_creatures", TITLE: "Wretched Creatures", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "the_human_nature", TITLE: "The Human Nature", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
+         {FILENAME: "fifty_princesses", TITLE: "Fifty Princesses", RETURNTO: "writing", RETURNTEXT: "Return to Writing", MATHJAX: False},
          )
           
           
@@ -45,9 +45,9 @@ for page in pages:
     
     html.write("\t<title>" + page[TITLE] + "</title>\n\n")
     html.write("\t<meta charset=\"UTF-8\">\n\n")
-    html.write("\t<link rel=\"shortcut icon\" href=\"Favicon.svg\">\n")
-    html.write("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"Dark.css\" title=\"Dark\">\n")
-    html.write("\t<link rel=\"alternate stylesheet\" type=\"text/css\" href=\"Light.css\" title=\"Light\">\n\n")
+    html.write("\t<link rel=\"shortcut icon\" href=\"favicon.svg\">\n")
+    html.write("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"dark.css\" title=\"Dark\">\n")
+    html.write("\t<link rel=\"alternate stylesheet\" type=\"text/css\" href=\"light.css\" title=\"Light\">\n\n")
     
     if page[MATHJAX]:
         html.write("\t<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>\n")
